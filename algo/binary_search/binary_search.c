9d17954a80ca19454231f0579ff938a2923428bb
mercydean <shao.dian@foxmail.com>
2022-08-20 05:50:48 +0900

    二分查找, 注意用集合和值域的观点结合具体例子抽象地看

--- algo/binary_search/binary_search.c
+++ algo/binary_search/binary_search.c
@@ -33,20 +33,18 @@ int search(int* nums, int numsSize, int target){
 
 /*普遍理解是左闭右开的区间.循环到最后, [left, right)会移到对应的位置.
    left趋向于指向第一个目标值, right则趋向第一个出现的目标值. 
-   但这个太过于概括,没有抓到本质.
 */
 int left_bound(int *nums, int numsSize, int target) {
     if (numsSize == 0) return -1;
     int left = 0;
     int right = numsSize; 
 	
-	/* 每次迭代实际上都是试图扩充左右两类值域, 直到左右两个值域相遇就说明解析完毕: 当left与right相遇, 相遇点就是结果.
+	/* 每次迭代实际上都是试图减半[left, right), 这可以扩充左右两类值域, 直到左右两个值域相遇, 说明解析完毕.
 	[-infinity, left) [left, right) [right, +infinity].
-	(等价于[left, right)为空: left = right也就是常说的左开右闭为空.)
+	left== right时[left, right)为空.
 	核心是[right, +infinity]的right逼近小于target的值但不超过它. 
 	[-infinity, left)的left试图逼近但不超过target.
-	可以证明, 如果存在这个target, 在最后一步left或者right就会相遇于第一个target. 否则将相遇于其余位置这时就只需要判断下标和和是否合法.
-	这就是为什么left初始为0, right初始为numsSize.
+	left== right时可能会溢出边界也可能对应的值不是target所以要做检查.
 	*/
 	
     while (left < right) {	// 区间左右相等时, 此区间就为空没有必要继续了.
@@ -60,9 +58,9 @@ int left_bound(int *nums, int numsSize, int target) {
         }
     }
 	
-	// 若没有对应的值, left(right)就是numsSize
+	// 序号有效性检查
 	if (left == numsSize) return -1;
-	// 若值为可能的, 就返回这个序号.
+	// 值可能不是想要的.
 	return nums[left] == target? left : -1;
 }
 
@@ -84,9 +82,9 @@ int right_bound(int *nums, int numsSize, int target) {
         }
     }
 	
-	// 若没有对应的值, left就是0.
+	// 序号有效性检查
 	if (left == 0) return -1;
-	// 最后返回这个序号.
+	// 值可能不是想要的.
 	return nums[left-1] == target? left-1 : -1;
 }
 
