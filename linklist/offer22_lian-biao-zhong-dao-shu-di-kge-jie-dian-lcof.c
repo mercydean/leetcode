759959f1d3c1fc6a001a4bc32891e7a9d03df887
mercydean <shao.dian@foxmail.com>
2022-08-29 22:40:36 +0900

    快慢指针, 快指针先走, 慢指针后走.

--- linklist/offer22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
+++ linklist/offer22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
@@ -12,12 +12,17 @@ struct ListNode* getKthFromEnd(struct ListNode* head, int k){
 
     if(head == NULL) return NULL;
 
-    while(k-- && slow != NULL)
+    while(k && slow != NULL)
     {
         slow = slow->next;
+        k--;
     }
 
-    //如果链表长度不够k, 就返回NULL;
+    /*
+     * 如果链表长度不够k, 就返回NULL.
+     * k     3    2    1     0
+     * node  . -> . -> . -> NULL
+     */
     if(k > 0) return NULL;
 
     while(slow != NULL)
