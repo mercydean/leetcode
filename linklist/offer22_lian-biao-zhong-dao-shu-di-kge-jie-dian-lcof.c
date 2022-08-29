ce81e4c6edd359343cfeb6f2aed39a6de7fee26a
mercydean <shao.dian@foxmail.com>
2022-08-29 22:27:29 +0900

    快慢指针遍历链表

---
+++ linklist/offer22_lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.c
@@ -0,0 +1,29 @@
+/*
+ * @problem		https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ * @solution	
+ * @brief		快慢指针, 快指针先走, 慢指针后走.
+ */
+
+
+struct ListNode* getKthFromEnd(struct ListNode* head, int k){
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    int count = k;
+
+    if(head == NULL) return NULL;
+
+    while(k-- && slow != NULL)
+    {
+        slow = slow->next;
+    }
+
+    //如果链表长度不够k, 就返回NULL;
+    if(k > 0) return NULL;
+
+    while(slow != NULL)
+    {
+        slow = slow->next;
+        fast = fast ->next;
+    }
+    return fast;
+}
